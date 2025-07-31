5a7c063e27d32c34435d00eb529492ab3a16c57f
Juan Cardona <juanscr24@hotmail.com>
2025-07-30 19:34:08 -0500

    first commit

---
+++ index.js
@@ -0,0 +1,33 @@
+// const http = require('http')
+// const fs = require('fs')
+
+// const server = http.createServer((req, res) => {
+//     const read = fs.createReadStream('./static/index.html')
+//     read.pipe(res)
+// })
+
+// server.listen(3000)
+// console.log(`Server on port ${3000}`);
+
+const express = require('express')
+
+const app = express()
+
+app.get('/', (req, res) => {
+    res.send('Hello world')
+})
+
+app.get('/about', (req, res) => {
+    res.send('About')
+})
+
+app.get('/weather', (req, res) => {
+    res.send('The current weather is Nice')
+})
+
+app.use((req, res) => {
+    res.status(404).send('No se encontro tu pagina')
+})
+
+app.listen(3000)
+console.log(`Server on port ${3000}`);
